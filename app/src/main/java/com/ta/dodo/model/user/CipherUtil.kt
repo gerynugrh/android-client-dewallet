21cbf715bcb1eb7bd3608de56ebd06ea0e959d77
Gery Wahyu <gerywahyunugraha@gmail.com>
2020-04-19 19:14:04 +0800

    Put util function as cipher companion object

--- app/src/main/java/com/ta/dodo/model/user/CipherUtil.kt
+++ app/src/main/java/com/ta/dodo/model/user/CipherUtil.kt
@@ -9,18 +9,22 @@ import javax.crypto.Cipher
 private const val provider = "AndroidKeyStoreBCWorkaround"
 private const val transformation = "RSA/ECB/PKCS1Padding"
 
-fun decrypt(encryptedBase64: String, privateKey: PrivateKey): String {
-    val encryptedBytes = Base64.decode(encryptedBase64, Base64.DEFAULT)
-    val cipher = Cipher.getInstance(transformation, provider)
-    cipher.init(Cipher.DECRYPT_MODE, privateKey)
+class CipherUtil {
+    companion object {
+        fun decrypt(encryptedBase64: String, privateKey: PrivateKey): String {
+            val encryptedBytes = Base64.decode(encryptedBase64, Base64.DEFAULT)
+            val cipher = Cipher.getInstance(transformation, provider)
+            cipher.init(Cipher.DECRYPT_MODE, privateKey)
 
-    return String(cipher.doFinal(encryptedBytes), StandardCharsets.UTF_8)
-}
+            return String(cipher.doFinal(encryptedBytes), StandardCharsets.UTF_8)
+        }
 
-fun encrypt(data: String, publicKey: PublicKey): String? {
-    val cipher = Cipher.getInstance(transformation, provider)
-    cipher.init(Cipher.ENCRYPT_MODE, publicKey)
+        fun encrypt(data: String, publicKey: PublicKey): String? {
+            val cipher = Cipher.getInstance(transformation, provider)
+            cipher.init(Cipher.ENCRYPT_MODE, publicKey)
 
-    val encryptedBytes = cipher.doFinal(data.toByteArray(StandardCharsets.UTF_8))
-    return Base64.encodeToString(encryptedBytes, Base64.DEFAULT)
+            val encryptedBytes = cipher.doFinal(data.toByteArray(StandardCharsets.UTF_8))
+            return Base64.encodeToString(encryptedBytes, Base64.DEFAULT)
+        }
+    }
 }
